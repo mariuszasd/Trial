5b2099a48c05738982d3415e6f7772ef4cb2add0
mariuszasd <mariuszpiwowarski3@gmail.com>
2019-03-05 17:23:38 +0100

    Rozpoczecie pracy nad lista

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
-    cout << "Testowanie tworzenia nowej branch-ki poleceniem git branch feature" << endl;
-    cout << "Aby przelaczyc sie na nowa branch-ke git checkout feature" << endl;
+    cout << " In this branch I will develop list " << endl;
     return 0;
 }
