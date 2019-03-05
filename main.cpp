8e2b4be537f4d5749c7c9e3ef51f95807169cd59
mariuszasd <mariuszpiwowarski3@gmail.com>
2019-03-05 17:12:13 +0100

    Testowa branch-ka feature

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "Testowanie tworzenia nowej branch-ki poleceniem git branch feature" << endl;
+    cout << "Aby przelaczyc sie na nowa branch-ke git checkout feature" << endl;
     return 0;
 }
