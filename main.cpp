8abb445521bfabd9a8a949f57189397d3be65189
marpivPraca <mariuszpiwowarski3@gmail.com>
2019-03-08 21:01:38 +0100

    Szablon kasy

--- main.cpp
+++ main.cpp
@@ -3,11 +3,24 @@
 
 using namespace std;
 
+template <typename T>
+class MyFirstTemplateClass {
+private:
+    T Value;
+public:
+    void SetValue(const T& newValue){Value = newValue;}
+    const T& GetValue() const {return Value;}
+};
+
 int main()
 {
     cout << "Hello World!" << endl;
 
     WrogiStatekKosmiczny* w_wrog = generujWroga();
     ulepszBron(w_wrog);
+    MyFirstTemplateClass <int> mHoldInteger;
+    mHoldInteger.SetValue(5);
+    cout << "Przechowywana wartosc to: " << mHoldInteger.GetValue() << endl;
+
     return 0;
 }
